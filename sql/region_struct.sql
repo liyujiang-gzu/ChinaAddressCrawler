7358cd14129d9c8a471715f84add5a6681bc7045
gzu-liyujiang <1032694760@qq.com>
2023-08-17 20:31:20 +0800

    乡级名称过长：中关村国家自主创新示范区大兴生物医药产业基地

--- sql/region_struct.sql
+++ sql/region_struct.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `region_stats`
     id          INT AUTO_INCREMENT PRIMARY KEY COMMENT '唯一编号',
     code        VARCHAR(12)                                   NOT NULL COMMENT '区划代码，只是精简的，不包括补齐的零',
     full_code   VARCHAR(12)                                   NULL COMMENT '完整的区划代码，省级代码+地级代码+县级代码+乡级代码+村级代码，不足则在末尾补零',
-    name        VARCHAR(20)                                   NOT NULL COMMENT '区划名称',
+    name        VARCHAR(25)                                   NOT NULL COMMENT '区划名称',
     level       ENUM ('省级', '地级', '县级', '乡级', '村级') NOT NULL COMMENT '区划级别，枚举值：省级、地级、县级、乡级、村级',
     parent_code VARCHAR(12)                                   NULL COMMENT '父级区划代码，只是精简的，不包括补齐的零',
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP            NOT NULL COMMENT '创建时间',
